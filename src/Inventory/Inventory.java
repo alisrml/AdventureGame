7d666dac3dce51d0790c58a2887009c6f9a78837
alisrml <117389464+alisrml@users.noreply.github.com>
2023-07-06 10:09:48 +0300

    Inventory ve Weapon Sınıfı

---
+++ src/Inventory/Inventory.java
@@ -0,0 +1,19 @@
+package Inventory;
+
+import Items.Weapon;
+
+public class Inventory {
+    private Weapon weapon;
+
+    public Inventory(){
+        this.weapon = new Weapon("Yumruk",0,0,0);
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+}
